0ed1ef5f508bcd7b06e54323a3a594cdfb6bd5c2
Dummy Datev <dummy@datev.de>
2019-12-11 15:01:52 +0100

    coverage

--- web/src/main/java/net/teamws/huemodoro/web/hue/HueBridge.java
+++ web/src/main/java/net/teamws/huemodoro/web/hue/HueBridge.java
@@ -88,31 +88,11 @@ public class HueBridge {
 	}
 
 	static class HueBody {
-		private boolean on = true;
-		private int sat = 254;
-		private int bri = 120;
-		private int hue = 5000;
-		private String effect = "none";
-
-		public boolean isOn() {
-			return on;
-		}
-
-		public int getSat() {
-			return sat;
-		}
-
-		public int getBri() {
-			return bri;
-		}
-
-		public int getHue() {
-			return hue;
-		}
-
-		public String getEffect() {
-			return effect;
-		}
+		public boolean on = true;
+		public int sat = 254;
+		public int bri = 120;
+		public int hue = 5000;
+		public String effect = "none";
 	}
 
 	static class HueLightsOffBody {
